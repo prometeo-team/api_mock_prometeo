d88392a0c91b95c2d2b84922aba423aca8897d96
gitForDavidL <davidtach26@gmail.com>
2024-09-19 22:47:20 -0500

    added env

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
